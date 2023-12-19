3e3df15fb65bf5e73220fb2e98c09ec5736acded
Giant Penguin <precioush2o@gmail.com>
2023-12-19 11:26:04 +0700

    Create website.cjs for use on Vercel
    add  website.cjs for use in Vercel's deployment step

---
+++ src/frontend/website.cjs
@@ -0,0 +1,25 @@
+const siteInfo = {
+    "name": "foundation-headless-cms",
+    "id": "21615c8b-365f-4e4c-ba79-45d57cc1fb1e",
+    "startPage": "99de17e6-0e5c-488f-93fa-15e7e4d9f660",
+    "startPageUrl": "http://w2k12sonarqube.internal.optimizely.com:8999/en/",
+    "locales": [
+        "en"
+    ],
+    "defaultLocale": "en",
+    "labels": [
+        {
+            "code": "en",
+            "label": "English"
+        }
+    ],
+    "primaryDomain": "http://w2k12sonarqube.internal.optimizely.com:8999",
+    "domains": [
+        "http://w2k12sonarqube.internal.optimizely.com:8001",
+        "http://localhost:8000",
+        "http://w2k12sonarqube.internal.optimizely.com:8999"
+    ],
+    "localeDomains": []
+};
+
+module.exports = siteInfo;
